8ced8a48c10c34ab99202d2819e01a9652e24929
Lucas Macarini <contatolucasmacarini@gmail.com>
2026-05-31 15:17:04 -0300

    Fix: API URL consume 2

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173, https://front-tarefas-chi.vercel.app",
+    origin: ["http://localhost:5173", "https://front-tarefas-chi.vercel.app"],
   }),
 );
 app.use(express.json());
